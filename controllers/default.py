3f63b4f126a78ced0d272476167e25a92f296709
Jeremy Hager <JHager@gongos.com>
2018-04-11 17:21:53 -0400

    echarts in

--- controllers/default.py
+++ controllers/default.py
@@ -3,7 +3,38 @@ def index():
 
 
 def get_data():
-    return 'Axios is initialized'
+    import pandas
+    import numpy as np
+    from gluon.serializers import json
+
+    df = pandas.read_csv('C:\\Work\\web2py\\applications\\bootcamp_template\\private\\data\\dataset_1\\Video_Games_Sales_as_at_22_Dec_2016.csv')
+
+    output = {}
+
+    name_list = df.Name.value_counts()
+    platform_list = df.Platform.value_counts()
+    genre_list = df.Genre.value_counts()
+    publisher_list = df.Publisher.value_counts()
+    developer_list = df.Developer.value_counts()
+
+    
+    chart1_data = []
+    chart1_labels = []
+    for i, row in df.iterrows():        
+        # chart3d_data.append([row[5], row[6], row[7]])
+        chart1_data.append([row[5], row[6]])
+        chart1_labels.append(row[0])
+    
+    # output = [{"Wii Sports": [41.36, 28.96], "Super Mario Bros.": [29.08, 3.58], "Mario Kart Wii": [15.68, 12.76]}]
+
+    output = chart1_data
+
+    # output['chart1_data'] = chart1_data
+    # output['chart1_labels'] = chart1_labels
+    # output = chart3d_labels
+    # output['chart3d_labels'] = chart3d_labels
+    # import ipdb;ipdb.set_trace()
+    return json(output)
 
 
 def user():
--- static/js/app/app.js
+++ static/js/app/app.js
@@ -1,10 +1,42 @@
 const app = new Vue({
-  // create your Vue Object
-  el: '#main'
+	// create your Vue Object
+	
 })
 
 // use axios for xhr
-axios.get(GETDATA)
-  .then(response => {
-    document.getElementById('main').innerHTML = response.data
+axios.get(GETDATA)	    
+	.then(output => {
+		// debugger;
+		
+        var chart_output = output['data'];
+
+        var chart_vue = new Vue({
+			// create your Vue Object
+			el: '#main',
+			data:{
+			  title: ""
+			},
+			methods:{	    
+			    build_chart:function(d){		        
+			        var myChart = echarts.init(document.getElementById('main'));
+					// debugger;
+
+					var data = chart_output;
+
+			        option = {
+					    xAxis: {},
+					    yAxis: {},
+					    series: [{
+					        symbolSize: 20,
+					        data: data,
+					        type: 'scatter'
+					    }]
+					};
+					myChart.setOption(option);
+			    }
+			}
+
+
+		})
+
   })
