47dc4a869831e29890119cdf27b5173014eccb26
Jeremy Hager <JHager@gongos.com>
2018-04-13 09:25:03 -0400

    charts up and running

--- static/js/app/app.js
+++ static/js/app/app.js
@@ -8,7 +8,9 @@ axios.get(GETDATA)
 	.then(output => {
 		// debugger;
 		
-        var chart_output = output['data'];
+        var scatter_output = output['data']['scatter_data'];
+        var line_output = output['data']['line_data'];
+        var pie_output = output['data']['pie_data'];
 
         var chart_vue = new Vue({
 			// create your Vue Object
@@ -17,22 +19,114 @@ axios.get(GETDATA)
 			  title: ""
 			},
 			methods:{	    
-			    build_chart:function(d){		        
-			        var myChart = echarts.init(document.getElementById('main'));
-					// debugger;
-
-					var data = chart_output;
+			    build_scatter:function(d){		        
+			        var scatterChart = echarts.init(document.getElementById('scatter'));
+					var scatter_data = scatter_output;					
 
 			        option = {
-					    xAxis: {},
-					    yAxis: {},
+			        	title: {
+		                    text: 'Bethesda North American/European Sales'
+		                },
+					    xAxis: {
+					    	name: 'NA Sales(in millions)',
+					    	nameLocation: 'end'
+					    },
+					    yAxis: {
+					    	name: 'EU Sales(in millions)',
+					    	nameLocation: 'end'
+					    },
+					    tooltip : {                    
+		                    showDelay : 0,
+		                    formatter : function (params) {
+		                        if (params.value.length > 1) {
+		                            return params.value[2] + ':<br/>NA Sales: ' + params.value[0] + '<br/>EU Sales: ' + params.value[1];
+		                        }
+		                        else {
+		                            return params.value[2] + ':<br/>NA Sales: ' + params.value[0] + '<br/>EU Sales: ' + params.value[1];
+		                        }
+		                    },
+		                    axisPointer:{
+		                        show: true,
+		                        type : 'cross',
+		                        lineStyle: {
+		                            type : 'dashed',
+		                            width : 1
+		                        }
+		                    }
+		                },
 					    series: [{
 					        symbolSize: 20,
-					        data: data,
+					        data: scatter_data,
 					        type: 'scatter'
 					    }]
 					};
-					myChart.setOption(option);
+					scatterChart.setOption(option);
+			    },
+			    build_line:function(d){		        
+			        var lineChart = echarts.init(document.getElementById('line'));
+					
+					var line_data = line_output;
+			        var labels = line_data['labels'];
+			        var values = line_data['values'];
+
+		            option = {
+		                title: {
+		                    text: 'Bethesda North American Sales'
+		                },
+		                tooltip: {
+		                    axisPointer:{
+		                        show: true,
+		                        type : 'cross',
+		                        lineStyle: {
+		                            type : 'dashed',
+		                            width : 1
+		                        }
+		                    }
+		                },
+		                xAxis: {
+		                    type: 'category',
+		                    boundaryGap: true,
+		                    data: labels,
+		                    show: false
+		                },
+		                yAxis: {
+		                    type: 'value',
+		                    name: 'NA Sales(in millions)',
+					    	nameLocation: 'end'
+		                },
+		                series: [{
+		                    // options for smoothing line and area fill below
+		                    smooth: true,
+		                    areaStyle: {},
+		                    ///////
+		                    symbolSize: 10,
+		                    showAllSymbol: true,
+		                    data: values,
+		                    type: 'line'
+		                }]
+		            };
+					lineChart.setOption(option);
+			    },
+			    build_pie:function(d){		        
+			        var pieChart = echarts.init(document.getElementById('pie'));					
+					var pie_data = pie_output;			        
+		            
+		            var option = {
+		                title: {
+		                    text: 'Bethesda North American Sales'
+		                },
+		                tooltip: {},
+		                legend: {
+		                    data: 'Legend'
+		                },                                              
+		                series: [{                    
+		                    name: 'NA Sales(in millions)',
+		                    type: 'pie',     
+		                    selectedMode: 'single',               
+		                    data: pie_data                    
+		                }]
+		            };
+					pieChart.setOption(option);
 			    }
 			}
 
